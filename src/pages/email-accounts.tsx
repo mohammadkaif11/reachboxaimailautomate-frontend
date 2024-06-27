3a3fc99eb4dd35ea3a76f64a40bfdfd49d56dc2d
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-06-27 19:22:04 +0530

    fixed the email issue

--- src/pages/email-accounts.tsx
+++ src/pages/email-accounts.tsx
@@ -101,7 +101,7 @@ function Page() {
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" className="px-6 py-3">
-                  Name
+                  Account Id
                 </th>
                 <th scope="col" className="px-6 py-3">
                   Email
@@ -126,7 +126,7 @@ function Page() {
                   >
                     Apple MacBook Pro
                   </th> */}
-                  <td className="px-6 py-4">{data?.name}</td>
+                  <td className="px-6 py-4">{data?.id}</td>
                   <td className="px-6 py-4">{data?.email}</td>
                   <td className="px-6 py-4">{data?.type}</td>
                   <td className="px-6 py-4">
